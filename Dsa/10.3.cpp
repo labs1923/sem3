d1a9c3ba8b149a27387d7b7ebdc35b53d8aefe74
Rakesh <60481830+deephunt3r@users.noreply.github.com>
2021-04-18 21:57:04 +0530

    Update 10.3.cpp

--- Dsa/10.3.cpp
+++ Dsa/10.3.cpp
@@ -2,126 +2,175 @@
 using namespace std;
 struct node {
     int data;
-    struct node *left;
     struct node *right;
+    struct node *left;
 };
-struct node *start = NULL;
-int insert(int data);
-void inorder(struct node *temp);
-int search(int data);
+struct node *start=NULL;
+int insert();
+int search();
+void inorder(struct node *);
+void del(int data);
+int min(struct node *);
 int main(){
-    int data,i,n,c=-1,r;
-    cout<<"enter the no. of elements"<<endl;
-    cin>>n;
-    cout<<"enter the elements"<<endl;
-    for(i=0;i<n;i++){
-    cin>>data;
-    insert(data);
+    int c=-1,d,data;
+    while(1){
+       cout<<"\n0.exit\n1.insert\n2.display\n3.search\n4.delete"<<endl;
+       cin>>c;
+       switch(c){
+           case 1: insert();
+                   break;
+           case 2: inorder(start);
+                   break;
+           case 3: d = search();
+                   if(d!=-1){ cout<<"element found at "<<d<<endl;}
+                   else {cout<<"not found"<<endl;}
+                   break;
+           case 4: cin>>data;
+                  del(data);
+                  break;
+           case 0: exit(0);
+       }
     }
-    while(c!=0){
-        cout<<"0.Exit\n1.Inserting\n2.Searching\n3.Display\nenter your choice"<<endl;
-        cin>>c;
-            if(c==1){
-            cout<<"enter the data"<<endl;
-            cin>>data;
-            insert(data);
-            }
-            else if(c==2){
-            cout<<"enter the search element :"<<endl;
-            cin>>data;
-            r=search(data);
-            if(r==-1)
-            cout<<"search element "<<data<<" is not in the tree"<<endl;
-            else
-            cout<<"search element "<<data<<" is present in the tree at a depth of "<<r<<endl;
-            }
-            else if(c==3){
-                    cout<<"In order   : ";
-                    inorder(start);
-                    cout<<endl;                
-            }
-            else if(c!=0)
-            cout<<"enter correct input"<<endl;
-        
-    }
-    free(start);
-    return 0;
-    
-}
-int insert(int data)
-{
-  struct node *temp,*t;
-  t = (struct node*)malloc(sizeof(struct node));
-  t->data = data;
-  t->right = NULL;
-  t->left = NULL;
-  if(start == NULL)
-  {
-      start=t;
-      return 0;
-  }
-  temp = start;
-  for(;;){
-      if(t->data<temp->data)
-      {
-        if(temp->left == NULL)
-        {
-           temp->left = t;
-           return 0 ;
-        }
-        else
-        {
-           temp = temp->left;  
-        }
-      }
-      else
-      {
-        if(temp->right == NULL)
-        {
-           temp->right = t; 
-           return 0;
-        }
-        else
-        {
-           temp = temp->right;  
-        }  
-      }
-  }
 }
 
-void inorder(struct node *temp)
-{
-    if(temp->left!=NULL)
-    inorder(temp->left);
-    cout<<temp->data<<" ";
-    if(temp->right!=NULL)
-    inorder(temp->right);
+int insert(){
+    int data;
+    cin>>data;
+    struct node *t,*temp;
+    t = (struct node *)malloc(sizeof(struct node ));
+    t->data = data;
+    t->left = NULL;
+    t->right = NULL;
+    if(start==NULL){
+        start=t;
+        return 0;
+    }
+    temp = start;
+    for(;;){
+        if(t->data<temp->data){
+           if(temp->left == NULL){
+               temp->left = t;
+               return 0;
+           }
+           else{
+               temp=temp->left;
+           }
+        }
+        else{
+            if(temp->right == NULL){
+                temp->right = t;
+                return 0;
+            }
+            else{
+                temp=temp->right;
+            }
+        }
+    }
 }
-int search(int data){
-  int d=0;
-  struct node *temp;
-  temp = start;
-  for(;;)
-  {   
-    if(data>temp->data)
-    {
-     if(temp->right==NULL&&temp->left==NULL)
-     return -1;
-     else
-     temp = temp->right;
-     d++;
+
+ void inorder(struct node *ptr){
+     if(ptr->left!=NULL)
+     inorder(ptr->left);
+     cout<<ptr->data<<" ";
+     if(ptr->right!=NULL)
+     inorder(ptr->right);
+ }
+
+int search(){
+    int data,d=0;
+    cin>>data;
+    struct node *temp;
+    temp = start;
+    while(1){
+    if(temp->data == data){
+        return d;
     }
-    else  if(data<temp->data)
-    {
-     if(temp->right==NULL&&temp->left==NULL)
-     return -1;
-     else
-     temp = temp->left;
-     d++;
+    else if(data>temp->data){
+        if(temp->right==NULL&&temp->left==NULL){
+            return -1;
+        }
+        else{
+            temp = temp->right;
+            d++;
+        }
     }
-    else if(data==temp->data)
-     return d;
-    else
-     return -1;
-  }
+    else{
+        if(temp->right==NULL && temp->left == NULL){
+            return -1;
+        }
+        else {
+            temp = temp->left;
+            d++;
+        }
+    }
+    }
+}
+
+void del(int data){
+    int r;
+    struct node *temp,*pt;
+    temp = start;
+    while(1){
+    if(temp->data == data){
+        if(temp->left==NULL&&temp->right==NULL){
+            if(pt->data>temp->data){
+                pt->left=NULL;
+            }
+            else
+            {
+                pt->right = NULL;
+            }
+            
+            free(temp);
+            return;
+        }
+        else if(temp->left==NULL||temp->right==NULL){
+            if(temp->left==NULL){
+                if(data>pt->data)
+                pt->right = temp->right;
+                else
+                pt->left = temp->right;     
+            }
+            else{
+                if(data>pt->data)
+                pt->right = temp->left;
+                else
+                pt->left = temp->left;
+            }
+            return ;
+        }
+        else{
+               r = min(temp->right);
+               del(r);
+               temp->data = r;
+               return ;
+        }
+    }
+    else if(data>temp->data){
+        if(temp->right==NULL&&temp->left==NULL){
+            return ;
+        }
+        else{
+            pt = temp;
+            temp = temp->right;
+        }
+    }
+    else{
+        if(temp->right==NULL && temp->left == NULL){
+            return ;
+        }
+        else {
+            pt = temp;
+            temp = temp->left;
+        }
+    }
+    
+    }
+}
+int min(struct node *ptr){
+    while(ptr->left!=NULL){
+        ptr = ptr->left;
+    }
+    return ptr->data;
+
 }
