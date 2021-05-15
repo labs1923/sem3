b17029f46afdb065fffe7373b292ff4bbed0e86f
sakethvishnu <60980466+sakethvishnu@users.noreply.github.com>
2021-05-15 10:56:49 +0530

    Create week_1.1.1.java

---
+++ Java/week_1.1.1.java
@@ -0,0 +1,8 @@
+//Accept two strings from the user and print it on console with concatenation of “and” in the middle of the strings using command line.
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println(args[0]+" and "+args[1]);  // args[0] and args[1] are inputs from command line.
+	}
+}
