b2fba1018470dd9bf5e3e38838d469e2c5c42202
sakethvishnu <60980466+sakethvishnu@users.noreply.github.com>
2021-05-15 11:19:18 +0530

    Create Week_1.2.3.java

---
+++ Java/Week_1.2.3.java
@@ -0,0 +1,19 @@
+//Accept a number n and print the list of n Fibonacci terms recursively using scanners.
+//Recurssion.
+
+import java.util.Scanner;
+ public class Main
+{
+    public static int fib_rec(int x){
+        return (x == 0 || x == 1)?x:fib_rec(x-1)+fib_rec(x-2);
+    }
+    
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		int n1 = 0,n2 = 1,i=0,n = sc.nextInt();
+		while(i<n){
+		    System.out.print(fib_rec(i)+" ");
+		    i++;
+		}
+	}
+}
