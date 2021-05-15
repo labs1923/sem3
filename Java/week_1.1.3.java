883d9dae87ce1e014a829e6fecdadff2ae12c717
sakethvishnu <60980466+sakethvishnu@users.noreply.github.com>
2021-05-15 11:16:50 +0530

    Create week_1.1.3.java

---
+++ Java/week_1.1.3.java
@@ -0,0 +1,17 @@
+//Accept a number n and print the list of n Fibonacci terms recursively using command line.
+//Recursion.
+
+public class Main
+{
+    public static int fib_rec(int x){
+        return (x == 0 || x == 1)?x:fib_rec(x-1)+fib_rec(x-2);
+    }
+    
+	public static void main(String[] args) {
+		int n1 = 0,n2 = 1,i=0;
+		while(i<Integer.parseInt(args[0])){
+		    System.out.print(fib_rec(i)+" ");
+		    i++;
+		}
+	}
+}
