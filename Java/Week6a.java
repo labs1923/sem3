7d578856117631f8b3074370501383ff9dd2c592
Mrdecent08 <60995685+Mrdecent08@users.noreply.github.com>
2021-06-22 21:54:32 +0530

    Add files via upload

---
+++ Java/Week6a.java
@@ -0,0 +1,26 @@
+public class Week6a{
+    public static void main(String[] args)
+    {
+        Cost c = new Cost();
+        c.display();
+    }
+}
+class Vehicle{
+    String v="Car";
+}
+class Brand extends Vehicle{
+    String[] b={"Ferrari sf90 stradale","Mercedes Benz","Bugatti","Bentley Turbo R","Lamborghini"};
+
+}
+class Cost extends Brand{
+    int r[]={75000000,5000000,55000000,65000000,95000000};
+    Cost(){     
+    }
+    void display(){
+        System.out.println("Vehicle Type : " +v);
+        for(int i=0;i<5;i++){  
+            System.out.println("Brand of the vehicle :" +b[i]);
+            System.out.println("Cost of the vehicle : "+r[i]);
+        }
+    }
+}
---
+++ Java/Week6b.java
@@ -0,0 +1,44 @@
+class Week62{
+    public static void main(String[] args){
+        Figure_3D[] obj3D = new Figure_3D[4];
+        obj3D [0] = new Cylinder();
+        obj3D [1] = new Cone();
+        obj3D [2] = new Sphere();
+        obj3D [3] = new Cube();
+        obj3D [0].volume();
+        obj3D [1].volume();
+        obj3D [2].volume();
+        obj3D [3].volume();
+    }
+}
+class Figure_3D{
+    void volume(){
+    }
+}
+class Cylinder extends Figure_3D{
+    void volume(){
+        int radius=5,height=10;
+        System.out.println("Volume of cylinder = "+(3.14*radius*radius*height));
+    }
+}
+class Cone extends Figure_3D{
+    void volume(){
+        int radius=5,height=6;
+        System.out.println("Volume of cone = " +((3.14*radius*radius*height)/3));
+    }
+}
+class Sphere extends Figure_3D{
+    void volume(){
+        int radius=3;
+        System.out.println("Volume of sphere = "+((4*3.14*radius*radius*radius)/3));
+    }
+}
+class Cube extends Figure_3D{
+    void volume(){
+        int side=9;
+        System.out.println("Volume of cube = " + (side*side*side));
+    }
+}
+
+
+
